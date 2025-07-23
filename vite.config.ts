cfd16446334d146e3e18de1f4e4537ab3dda8ded
elreco <elreco@users.noreply.github.com>
2025-07-23 21:50:02 +0200

    Update vite.config.ts - Version 1

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  base: "./",
+  build: {
+    minify: false,
+    sourcemap: false,
+    cssCodeSplit: false,
+    target: "esnext",
+    ssr: false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})

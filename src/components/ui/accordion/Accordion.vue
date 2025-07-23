449e42d434dac01b178cd5b52d00d453efd02ecc
elreco <elreco@users.noreply.github.com>
2025-07-23 21:50:29 +0200

    Update src/components/ui/accordion/Accordion.vue - Version 1

---
+++ src/components/ui/accordion/Accordion.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { provide, ref } from "vue";
+
+const props = defineProps<{
+  type?: "single" | "multiple";
+  collapsible?: boolean;
+  defaultValue?: string | string[];
+}>();
+
+const modelValue = ref<string | string[]>(props.defaultValue || (props.type === "multiple" ? [] : ""));
+
+provide("accordion", {
+  type: props.type || "single",
+  collapsible: props.collapsible || false,
+  modelValue,
+  updateModelValue: (value: string | string[]) => {
+    modelValue.value = value;
+  }
+});
+</script>
+
+<template>
+  <div class="w-full">
+    <slot />
+  </div>
+</template>

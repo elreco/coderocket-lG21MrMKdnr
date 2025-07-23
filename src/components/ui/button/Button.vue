109a128c7bdc478a7dbbfde3177afff14d4eedf7
elreco <elreco@users.noreply.github.com>
2025-07-23 21:50:21 +0200

    Update src/components/ui/button/Button.vue - Version 1

---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { buttonVariants } from ".";
+import { cn } from "@/lib/utils";
+import { type VariantProps } from "class-variance-authority";
+
+interface ButtonProps extends VariantProps<typeof buttonVariants> {
+  type?: "button" | "submit" | "reset";
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  type: "button",
+  variant: "default",
+  size: "default",
+});
+</script>
+
+<template>
+  <button
+    :type="type"
+    :class="cn(buttonVariants({ variant, size }), $attrs.class ?? '')"
+  >
+    <slot />
+  </button>
+</template>

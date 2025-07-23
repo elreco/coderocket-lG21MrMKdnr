3020fcfdcd658ac1e752df9653cf759a0d1fc3ee
elreco <elreco@users.noreply.github.com>
2025-07-23 21:50:05 +0200

    Update src/router/index.ts - Version 1

---
+++ src/router/index.ts
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/pages/HomePage.vue'
+import AboutPage from '@/pages/AboutPage.vue'
+import ServicesPage from '@/pages/ServicesPage.vue'
+import CaseStudiesPage from '@/pages/CaseStudiesPage.vue'
+import InsightsPage from '@/pages/InsightsPage.vue'
+import ContactPage from '@/pages/ContactPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutPage
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    component: ServicesPage
+  },
+  {
+    path: '/case-studies',
+    name: 'CaseStudies',
+    component: CaseStudiesPage
+  },
+  {
+    path: '/insights',
+    name: 'Insights',
+    component: InsightsPage
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: ContactPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0 }
+  }
+})
+
+export default router
